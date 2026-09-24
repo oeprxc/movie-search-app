b3e77fce6b8d9e97704b0510118f0bafed6477ec
oeprxc <oeprxc@outlook.com>
2026-09-24 13:51:05 -0700

    Add movie search form validation

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -3,16 +3,24 @@ import { useState } from "react"
 
 
  const SearchBar = () => {
-    const [movieName, setMovie] = useState ()
+    const [movieName, setMovie] = useState("")
+
     const handleSubmit = (event) => {
    event.preventDefault()
+   console.log(movieName)
+   const userInput = movieName.trim()
+   if(userInput === "") {
+  alert("Please enter a valid search")
+  return
+   }
+
     }
 
     return(
     <>
     <div className="searchSection">
         <form action="" onSubmit={handleSubmit}>
-   <input type="text"  id="searchMovie" value={movieName} placeholder="Search Movie" />
+   <input type="text"  id="searchMovie" value={movieName} placeholder="Search Movie" onChange={(e) => setMovie(e.target.value)}/>
     <button id="btn">Search Movie</button>
 
         </form>
